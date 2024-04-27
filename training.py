0372428bdb9021c0abb2dd27ea5dbbad33667ae0
helena <helenaff13@gmail.com>
2024-04-27 16:31:16 -0300

    added neural network model build

--- training.py
+++ training.py
@@ -61,3 +61,18 @@ training_data = np.array(training_data)
 trainX = training_data[:, :len(all_patterns_words)]
 trainY = training_data[:, len(all_patterns_words):]
 
+# build neural network model
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
+
+sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(trainX, trainY, epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5')
+print('Done')
