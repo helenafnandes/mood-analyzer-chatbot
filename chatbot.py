c91840e6065a1179577dbb64a25e2291fca11d10
helena <helenaff13@gmail.com>
2024-05-23 19:58:42 -0300

    refactored chatbot to class Chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,128 +1,76 @@
-# Suppress TensorFlow's log messages for cleaner chatbot interaction
+# chatbot.py
 import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow's log messages
-
 import random
 import json
 import pickle
 import numpy as np
 import nltk
-from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
-
-from sentiment_analysis import analyze_sentiment
-from spell_corrector import spell_check
-
-# Constants
-INTENTS_FILE = 'intents.json'
-WORDS_FILE = 'allPatternsWords.pkl'
-CLASSES_FILE = 'allIntentsTags.pkl'
-MODEL_FILE = 'chatbot_model.keras'
-ERROR_THRESHOLD = 0.25
+from nltk.stem import WordNetLemmatizer
 
 tf.keras.utils.disable_interactive_logging()
 
-lemmatizer = WordNetLemmatizer()
+class Chatbot:
+    def __init__(self):
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+        self.INTENTS_FILE = 'intents.json'
+        self.WORDS_FILE = 'allPatternsWords.pkl'
+        self.CLASSES_FILE = 'allIntentsTags.pkl'
+        self.MODEL_FILE = 'chatbot_model.keras'
+        self.ERROR_THRESHOLD = 0.25
+        self.lemmatizer = WordNetLemmatizer()
+        self.words = None
+        self.classes = None
+        self.model = None
+        self.intents_data = None
+        self.load_data()
 
+    def load_data(self):
+        with open(self.INTENTS_FILE, 'r') as file:
+            self.intents_data = json.load(file)
+        with open(self.WORDS_FILE, 'rb') as file:
+            self.words = pickle.load(file)
+        with open(self.CLASSES_FILE, 'rb') as file:
+            self.classes = pickle.load(file)
+        self.model = tf.keras.models.load_model(self.MODEL_FILE)
 
-# Load intents data
-def load_intents(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
+    def tokenize_sentence(self, sentence):
+        sentence_words = nltk.word_tokenize(sentence)
+        return [self.lemmatizer.lemmatize(word) for word in sentence_words]
 
+    def bag_of_words(self, sentence):
+        sentence_words = self.tokenize_sentence(sentence)
+        bag = [0] * len(self.words)
+        for i, word in enumerate(self.words):
+            if word in sentence_words:
+                bag[i] = 1
+        return np.array(bag)
 
-# Load words and classes
-def load_data(file_path):
-    with open(file_path, 'rb') as file:
-        return pickle.load(file)
+    def predict_top_intent(self, sentence):
+        bow = self.bag_of_words(sentence)
+        res = self.model.predict(np.array([bow]))[0]
+        results = [[i, r] for i, r in enumerate(res) if r > self.ERROR_THRESHOLD]
+        results.sort(key=lambda x: x[1], reverse=True)
+        sentence_intents = [{'intent': self.classes[r[0]], 'probability': str(r[1])} for r in results]
+        return sentence_intents[0]['intent']
 
+    def get_response_by_intent(self, sentence_intent):
+        responses = [intent['responses'] for intent in self.intents_data['intents'] if intent['tag'] == sentence_intent]
+        flat_responses = [response for sublist in responses for response in sublist]
+        return random.choice(flat_responses)
 
-# Load trained model
-def load_model(file_path):
-    return tf.keras.models.load_model(file_path)
+    def get_welcome_message(self):
+        return "Welcome to our bakery chatbot! 🍰🍩 I'm here to assist you with any questions you have about our delicious treats and services. Feel free to ask me anything, from information about our products to placing an order. Let's get started! How can I assist you today?"
 
+    '''
+    def send_message(self, message):
+        corrected_message = spell_check(message)
+        sentence_top_intent = self.predict_top_intent(corrected_message)
+        response = self.get_response_by_intent(sentence_top_intent)
+        sentiment = analyze_sentiment(corrected_message)
+        return [corrected_message, response, sentiment]
+    '''
 
-# Tokenize sentence
-def tokenize_sentence(sentence, lemmatizer):
-    sentence_words = nltk.word_tokenize(sentence)
-    return [lemmatizer.lemmatize(word) for word in sentence_words]
-
-
-# Create bag of words
-def bag_of_words(sentence, words):
-    sentence_words = tokenize_sentence(sentence, lemmatizer)
-    bag = [0] * len(words)
-    for i, word in enumerate(words):
-        if word in sentence_words:
-            bag[i] = 1
-    return np.array(bag)
-
-
-# Predict class/intent
-def predict_intents(sentence, model):
-    bow = bag_of_words(sentence, words)
-    res = model.predict(np.array([bow]))[0]
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    results.sort(key=lambda x: x[1], reverse=True)
-    return [{'intent': classes[r[0]], 'probability': str(r[1])} for r in results]
-
-
-def predict_top_intent(sentence, model):
-    bow = bag_of_words(sentence, words)
-    res = model.predict(np.array([bow]))[0]
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    results.sort(key=lambda x: x[1], reverse=True)
-    sentence_intents = [{'intent': classes[r[0]], 'probability': str(r[1])} for r in results]
-    top_intent_tag = sentence_intents[0]['intent']
-    return top_intent_tag
-
-
-# Get response
-def get_response(sentence, intents_data):
-    sentence_intents = predict_intents(sentence, model)
-    top_intent_tag = sentence_intents[0]['intent']
-    responses = [intent['responses'] for intent in intents_data['intents'] if intent['tag'] == top_intent_tag]
-    flat_responses = [response for sublist in responses for response in sublist]
-    return random.choice(flat_responses)
-
-
-def get_response_by_intent(sentence_intent, intents_data):
-    responses = [intent['responses'] for intent in intents_data['intents'] if intent['tag'] == sentence_intent]
-    flat_responses = [response for sublist in responses for response in sublist]
-    return random.choice(flat_responses)
-
-
-# Main function
-def main():
-    global lemmatizer, words, classes, model
-
-    print("Welcome to our bakery chatbot! 🍰🍩"
-          "I'm here to assist you with any questions you have about our delicious treats and services. Feel free to ask me anything, from information about our products to placing an order."
-          "Let's get started! How can I assist you today?")
-
-    # Load data
-    intents_data = load_intents(INTENTS_FILE)
-    words = load_data(WORDS_FILE)
-    classes = load_data(CLASSES_FILE)
-    model = load_model(MODEL_FILE)
-
-    while True:
-        # Get and print response
-        sentence = input("You: ")
-        sentence = spell_check(sentence)
-        sentiment = analyze_sentiment(sentence)
-        if sentiment == 'Negative':
-            print("Your satisfaction is our priority. I'll make sure to escalate your concern to one of our "
-                  "attendants, who will assist you promptly. Please hang tight; we'll have someone with you shortly.")
-            break
-        else:
-            sentence_top_intent = predict_top_intent(sentence, model)
-            response = get_response_by_intent(sentence_top_intent, intents_data)
-            print("Bot: ", response)
-            if sentence_top_intent == 'goodbye':
-                break
-
-
-if __name__ == "__main__":
-    main()
+    def send_message(self, message):
+        sentence_top_intent = self.predict_top_intent(message)
+        return self.get_response_by_intent(sentence_top_intent)
