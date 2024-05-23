cd7a36db286d033baf5af5295f71c0bc730e9207
helena <helenaff13@gmail.com>
2024-05-23 19:59:16 -0300

    added sentiment analysis and spell checking functionalities

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, jsonify
+# app.py
+from flask import Flask, jsonify, request
+from chatbot import Chatbot
+
+from sentiment_analysis import analyze_sentiment
+from spell_corrector import spell_check
 
 app = Flask(__name__)
+chatbot = Chatbot()
+
 
 # Manually configure CORS headers
 @app.after_request
@@ -10,13 +17,30 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     return response
 
-# Example route
-@app.route('/api/data')
-def get_data():
-    data = {"message": "Hello from Flask!"}
-    print("Hello, world!")
-    return jsonify(data)
+
+# Chatbot route
+@app.route('/api/chatbot', methods=['POST'])
+def chatbot_endpoint():
+    data = request.json
+    message = data['message']
+    corrected_message = spell_check(message)
+    sentiment = analyze_sentiment(corrected_message)
+    if sentiment == 'Negative':
+        response = ("Your satisfaction is our priority. I'll make sure to escalate your concern to one of our "
+                    "attendants, who will assist you promptly. Please hang tight; we'll have someone with you shortly.")
+    else:
+        response = chatbot.send_message(corrected_message)
+    return jsonify({
+        'corrected_message': corrected_message,
+        'sentiment': sentiment,
+        'response': response
+    })
+
+
+@app.route('/api/welcome_message', methods=['GET'])
+def welcome_message():
+    return jsonify({"message": chatbot.get_welcome_message()})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
