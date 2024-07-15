496e08d88b848939f6d756479c219d185f4c3661
helena <helenaff13@gmail.com>
2024-07-15 12:08:41 -0300

    added nltk resources

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -2,25 +2,29 @@ import os
 import random
 import json
 import pickle
-import numpy as np
 import nltk
 import tensorflow as tf
+import numpy as np
 from nltk.stem import WordNetLemmatizer
+import logging
 
 from sentiment_analysis import analyze_sentiment
 from spell_corrector import spell_check
 
 tf.keras.utils.disable_interactive_logging()
 
+# Download necessary NLTK resources
+nltk.download('punkt')
+nltk.download('wordnet')
 
 class Chatbot:
     def __init__(self):
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-        self.INTENTS_FILE = '../personalization_files/intents.json'
+        self.INTENTS_FILE = 'personalization_files/intents.json'
         self.WORDS_FILE = './training_generated_files/allPatternsWords.pkl'
         self.CLASSES_FILE = './training_generated_files/allIntentsTags.pkl'
         self.MODEL_FILE = './training_generated_files/chatbot_model.keras'
-        self.MESSAGES_FILE = '../personalization_files/messages.json'
+        self.MESSAGES_FILE = 'personalization_files/messages.json'
         self.ERROR_THRESHOLD = 0.60
         self.lemmatizer = WordNetLemmatizer()
         self.words = None
@@ -29,6 +33,8 @@ class Chatbot:
         self.intents_data = None
         self.messages = None
         self.load_data()
+        logging.basicConfig(level=logging.DEBUG)
+        self.logger = logging.getLogger(__name__)
 
     def load_data(self):
         with open(self.INTENTS_FILE, 'r', encoding='utf-8') as file:
@@ -52,15 +58,15 @@ class Chatbot:
         for i, word in enumerate(self.words):
             if word in sentence_words:
                 bag[i] = 1
-        return np.array(bag)
+        return bag
 
     def predict_top_intent(self, sentence):
         bow = self.bag_of_words(sentence)
-        res = self.model.predict(np.array([bow]))[0]
+        bow = np.array(bow)  # Convert list to numpy array
+        res = self.model.predict(np.array([bow]))[0]  # Ensure input is in correct format
         results = [[i, r] for i, r in enumerate(res)]
         results.sort(key=lambda x: x[1], reverse=True)
         if results[0][1] > self.ERROR_THRESHOLD:
-            # print(results[0][1], self.classes[results[0][0]], results[1][1], self.classes[results[1][0]], results[2][1], self.classes[results[2][0]])
             return self.classes[results[0][0]]
         else:
             return "no_match"
@@ -78,19 +84,17 @@ class Chatbot:
     def get_negative_intent_response(self):
         return self.messages.get('negative_intent', "Default negative message response not found in JSON file")
 
-    '''
     def send_message(self, message):
-        corrected_message = spell_check(message)
-        sentence_top_intent = self.predict_top_intent(corrected_message)
-        response = self.get_response_by_intent(sentence_top_intent)
-        sentiment = analyze_sentiment(corrected_message)
-        return [corrected_message, response, sentiment]
-    '''
-
-    def send_message(self, message):
-        sentence_top_intent = self.predict_top_intent(message)
-        return self.get_response_by_intent(sentence_top_intent)
-
+        try:
+            self.logger.debug(f"Received message: {message}")
+            sentence_top_intent = self.predict_top_intent(message)
+            self.logger.debug(f"Predicted intent: {sentence_top_intent}")
+            response = self.get_response_by_intent(sentence_top_intent)
+            self.logger.debug(f"Generated response: {response}")
+            return response
+        except Exception as e:
+            self.logger.error(f"Error in send_message: {e}")
+            return "Sorry, I encountered an error processing your message."
 
 # Usage
 
@@ -101,10 +105,10 @@ if __name__ == "__main__":
         message = input("You: ")
         message = spell_check(message)
         sentiment = analyze_sentiment(message)
-        if(sentiment == 'Negative'):
+        if sentiment == 'Negative':
             response = bot.get_negative_intent_response()
         else:
             response = bot.send_message(message)
         print("Bot:", response)
-        if(bot.predict_top_intent(message) == 'goodbye'):
+        if bot.predict_top_intent(message) == 'goodbye':
             break
